87a19aba26244b20aaedfcdf5dc04d7aeb8e3a02
Omar Metwally <omar.metwally@gmail.com>
2013-12-17 23:40:37 -0800

    Added photo functionality

--- src/com/remedy/glass/MainActivity.java
+++ src/com/remedy/glass/MainActivity.java
@@ -1,7 +1,18 @@
 package com.remedy.glass;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -10,6 +21,7 @@ import android.view.MenuItem;
 import android.view.WindowManager;
 
 import com.google.android.glass.app.Card;
+import com.google.android.glass.media.Camera;
 
 /*** 
  MainActivity is started in LoginActivity after  QR code is successfully read 
@@ -19,8 +31,16 @@ import com.google.android.glass.app.Card;
 
 public class MainActivity extends LogoutOnStopActivity {
 	public static final String LOGIN_INTENT_EXTRA_CREDENTIALS = "login_credentials";
+	public static final int MEDIA_TYPE_IMAGE = 1;
+	public static final int MEDIA_TYPE_VIDEO = 2;
+	
 	protected static final String TAG = "Remedy.main";
 	private User currentUser;
+	private static final int CAPTURE_IMAGE_REQUEST_CODE = 100;
+	private static final int CAPTURE_VIDEO_REQUEST_CODE = 200;
+
+	private Uri mFileUri;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -92,6 +112,10 @@ public class MainActivity extends LogoutOnStopActivity {
             	closeOptionsMenu();
                 startActivity(new Intent(this, CallExpertActivity.class));
                 return true;
+            case R.id.take_photo_menu_item:
+            	Log.d(TAG, "Take photo menu item selected");
+            	startPhoto();
+            	return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -106,4 +130,91 @@ public class MainActivity extends LogoutOnStopActivity {
           }
           return false;
     }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	
+    	if (requestCode == CAPTURE_IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK ) {
+    		
+    		String thumbnailFilepath = data.getStringExtra(Camera.EXTRA_THUMBNAIL_FILE_PATH);
+    		String fullFilepath = data.getStringExtra(Camera.EXTRA_PICTURE_FILE_PATH);
+    		
+    		Log.d(TAG, "Intent extra: " + data.getExtras());
+    		Log.d(TAG, "Thumbnail file path: " + fullFilepath);
+    	}
+    	/*
+    	if (requestCode == CAPTURE_IMAGE_REQUEST_CODE) {
+    		if (resultCode == RESULT_OK) {
+    			Toast.makeText(this, "Image saved to: \n" + data.getData(), Toast.LENGTH_LONG).show();
+    			
+    		} else if (resultCode == RESULT_CANCELED) {
+    			// user cancelled the image capture
+    		} else {
+    			// image capture failed, advise user
+    		}
+    	}
+    	
+    	   if (requestCode == CAPTURE_VIDEO_REQUEST_CODE) {
+    	        if (resultCode == RESULT_OK) {
+    	            // Video captured and saved to fileUri specified in the Intent
+    	            Toast.makeText(this, "Video saved to:\n" +
+    	                     data.getData(), Toast.LENGTH_LONG).show();
+    	        } else if (resultCode == RESULT_CANCELED) {
+    	            // User cancelled the video capture
+    	        } else {
+    	            // Video capture failed, advise user
+    	        }
+    	    }
+    	    */
+    }
+    
+   	private void startPhoto() {
+	   
+   		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+   		startActivityForResult(intent, CAPTURE_IMAGE_REQUEST_CODE);
+	}
+   	
+   	private void uploadPhoto(String filePath) {
+   		File imgFile = new File(filePath);
+   		if (! imgFile.exists()) 
+   			return;
+   		
+   		Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+   		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+   		myBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+   		byte[] bitmapData = stream.toByteArray();
+   	}
+   	
+   	private static Uri getOutputMediaFileUri(int type) {
+   		return Uri.fromFile(getOutputMediaFile(type));
+   	}
+   	
+   	private static File getOutputMediaFile(int type) {
+   		
+   		File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyCameraApp");
+   		
+   		Log.d(TAG, "mediaStorageDir: " + mediaStorageDir.getPath());
+   		
+   		if (!mediaStorageDir.exists()) {
+   			if (!mediaStorageDir.mkdirs()) {
+   				Log.d(TAG, "failed to create directory");
+   				return null;
+   			}
+   		}
+   		
+   		// create a media file name
+   		String timeStamp = new SimpleDateFormat("yyMMdd_HHmmss").format(new Date());
+   		File mediaFile;
+   		if (type == MEDIA_TYPE_IMAGE) {
+   			mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+   			Log.d(TAG, "media file name: " + mediaFile.getAbsolutePath());
+   			
+   		} else if( type == MEDIA_TYPE_VIDEO) {
+   			mediaFile = new File(mediaStorageDir.getPath() + File.separator + "VID_" + timeStamp + ".mp4");
+   		} else {
+   			return null;
+   		}
+   		
+   		return mediaFile;
+   	}
 }
